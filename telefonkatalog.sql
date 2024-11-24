871237c2e7a7234cf2ce229046c36a099cb9b9a7
Jasan33 <156905772+Jasan33@users.noreply.github.com>
2024-11-24 04:18:32 +0100

    Add 2 more tables

--- telefonkatalog.sql
+++ telefonkatalog.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS personer (
 INSERT INTO personer (fornavn, etternavn, telefonnummer) 
 VALUES ('Ola', 'Nordmann', '12345678'),
        ('Kari', 'Nordmann', '87654321');
+
+CREATE TABLE IF NOT EXISTS passese (
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- Legg til en primærnøkkel
+    brukernavn VARCHAR(200),
+    dittpass VARCHAR(200) default NULL
+);
+
+INSERT INTO passese (brukernavn, dittpass)
+VALUES ('Olam',aes_encrypt('passorda', 'key1234'));
